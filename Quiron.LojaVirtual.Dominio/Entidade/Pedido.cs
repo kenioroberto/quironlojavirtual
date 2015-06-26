3c6f07c2b0606937d8154ef49357246dce8b0d2c
kenioroberto <kenioroberto@gmail.com>
2015-06-26 00:23:08 -0300

    Modelo ParaPedido

---
+++ Quiron.LojaVirtual.Dominio/Entidade/Pedido.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Entidade
+{
+    public class Pedido
+    {
+        [Required(ErrorMessage = "Informe seu Nome")]
+        public string NomeCliente { get; set; }
+
+        [Display(Name = "Cep:")]
+        public string Cep { get; set; }
+
+        [Required(ErrorMessage = "Informe seu Endereço")]
+        [Display(Name = "Endereço:")]
+        public string Endereco { get; set; }
+
+        [Display(Name = "Complemento:")]
+        public string Complemento { get; set; }
+
+        [Required(ErrorMessage = "Informe sua Cidade")]
+        [Display(Name = "Cidade:")]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "Informe seu Bairro")]
+        [Display(Name = "Bairro:")]
+        public string Bairro { get; set; }
+
+        [Required(ErrorMessage = "Informe seu Email")]
+        [Display(Name = "Email:")]
+        [EmailAddress(ErrorMessage = "E-mail Inválido")]
+        public string Email { get; set; }
+
+        public bool EmbrulhaPresente { get; set; }
+
+    }
+}
